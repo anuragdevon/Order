d508533b7da7746cd150a9d418cfadc53fb14799
anuragdevon <msm19b021@iiitdm.ac.in>
2023-05-25 13:57:34 +0530

    refactor: order repository tests

--- pkg/repository/order_test.go
+++ pkg/repository/order_test.go
@@ -4,50 +4,31 @@ import (
 	"testing"
 
 	"order/pkg/repository/models"
-
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
 )
 
 func TestRepository(t *testing.T) {
 	t.Run("CreateOrder method to successfully create database entry for new order", func(t *testing.T) {
-		testDB := setupTestDB()
-
-		handler := Database{DB: testDB}
-
 		order := &models.Order{
 			ItemId: 123,
 			UserId: 456,
 		}
-
-		err := handler.CreateOrder(order)
+		err := db.CreateOrder(order)
 		if err != nil {
 			t.Errorf("Failed to create order: %v", err)
 		}
 	})
 
-	t.Run("DeleteOrder to successfully delete an order for failure occured from InventorySvc", func(t *testing.T) {
-		testDB := setupTestDB()
+	t.Run("DeleteOrder to successfully delete an order from database", func(t *testing.T) {
 
-		handler := Database{DB: testDB}
+		order := &models.Order{
+			ItemId: 789,
+			UserId: 456,
+		}
+		db.CreateOrder(order)
 
-		orderID := int64(789)
-		err := handler.DeleteOrder(orderID)
+		err := db.DeleteOrder(order.Id)
 		if err != nil {
 			t.Errorf("Failed to delete order: %v", err)
 		}
 	})
-}
-
-func setupTestDB() *gorm.DB {
-	dbURL := "postgres://postgres:postgres@localhost:5432/testdb2?sslmode=disable"
-
-	testDB, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
-	if err != nil {
-		panic("Failed to connect to test database: " + err.Error())
-	}
-
-	testDB.AutoMigrate(&models.Order{})
-
-	return testDB
 }

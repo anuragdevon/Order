9a17cee5ae8d08f3de5d346a654c6aec19cd6aa2
anuragdevon <msm19b021@iiitdm.ac.in>
2023-05-25 11:03:38 +0530

    refactor: inventory.proto for quanity and remove non-required methods

--- pkg/service/order.go
+++ pkg/service/order.go
@@ -1,1 +1,46 @@
 package service
+
+import (
+	"context"
+	"net/http"
+
+	"order/pkg/client"
+	"order/pkg/pb"
+	"order/pkg/repository"
+	"order/pkg/repository/models"
+
+	"gorm.io/gorm"
+)
+
+type OrderService struct {
+	db           *gorm.DB
+	InventorySvc client.InventoryServiceClient
+}
+
+func (os *OrderService) CreateOrder(ctx context.Context, req *pb.CreateOrderRequest) (*pb.CreateOrderResponse, error) {
+	itemResp, err := os.InventorySvc.GetItem(req.ItemId)
+
+	if err != nil {
+		return &pb.CreateOrderResponse{Status: http.StatusBadRequest, Error: err.Error()}, nil
+	} else if itemResp.Status >= http.StatusNotFound {
+		return &pb.CreateOrderResponse{Status: itemResp.Status, Error: itemResp.Error}, nil
+	} else if itemResp.Data.Quantity < req.Quantity {
+		return &pb.CreateOrderResponse{Status: http.StatusConflict, Error: "Quantity is insufficient"}, nil
+	}
+
+	order := models.Order{
+		ItemId: itemResp.Data.Id,
+		UserId: req.UserId,
+	}
+	db := repository.Database{DB: os.db}
+
+	err = db.CreateOrder(&order)
+
+	if err != nil {
+		return &pb.CreateOrderResponse{Status: http.StatusBadRequest, Error: err.Error()}, nil
+	}
+	return &pb.CreateOrderResponse{
+		Status: http.StatusCreated,
+		Id:     order.Id,
+	}, nil
+}
